1bcf600ff29e8661c86bd31d924fd7858d146eed
Kevin Doyle <kevin.doyle@thinkbrownstone.com>
2017-05-26 11:11:30 -0400

    Initial commit, adding announcer component

---
+++ src/Announcer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Announcer extends Component {
+    static propTypes = {
+        text: PropTypes.string,
+        politeness: PropTypes.string,
+    }
+
+    static defaultProps = {
+        politeness: 'polite'
+    }
+
+    render() {
+        const { text, politeness } = this.props;
+
+        return (
+            <div aria-live={politeness}>
+                <p>{text}</p>
+            </div>
+        )
+    }
+}
+
+export default Announcer;
