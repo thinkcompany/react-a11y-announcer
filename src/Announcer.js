98c30258779bec332f88a3241f6d7b0b2b7bcabd
Joe Parisi <jpparisi@gmail.com>
2017-06-30 15:31:19 -0400

    Added defaultStyles for the element being visually hidden.

--- src/Announcer.js
+++ src/Announcer.js
@@ -11,11 +11,31 @@ class Announcer extends Component {
         politeness: 'polite'
     }
 
+    defaultStyles = {
+        position: 'absolute',
+        visibility: 'visible',
+        overflow: 'hidden',
+        display: 'block',
+        width: '1px',
+        height: '1px',
+        margin: '-1px',
+        border: '0',
+        padding: '0',
+        clip: 'rect(0px, 0px, 0px, 0px)',
+        clipPath: 'polygon(0px 0px, 0px 0px, 0px 0px, 0px 0px)',
+        whiteSpace: 'nowrap'
+    }
+
     render() {
-        const { text, politeness } = this.props;
+        const { className, text, politeness } = this.props;
+        const styles = className ? {} : this.defaultStyles;
 
         return (
-            <div aria-live={politeness}>
+            <div 
+                aria-live={politeness}
+                style={styles}
+                className={className}
+            >
                 {
                     text.length ?
                     <p>{text}</p> :
