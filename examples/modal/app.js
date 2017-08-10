a2a471b4e45b943ec433d2bc40ad9e74e97396ec
Luke Pettway <luke.pettway@thinkbrownstone.com>
2017-08-10 09:32:42 -0400

    Added starter content to other examples

---
+++ examples/modal/app.js
@@ -0,0 +1,40 @@
+
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Announcer from '../../src/Announcer';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      announcement: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      announcement: 'Here\'s a new announcement!'
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Announcer text={this.state.announcement} />
+        <div className="App-header">
+          <h2>Modal Example</h2>
+          <button type="button" onClick={this.handleClick}>Trigger new announcement</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('container'))
+
+if (module.hot) { module.hot.accept() }
---
+++ examples/notification/app.js
@@ -0,0 +1,39 @@
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Announcer from '../../src/Announcer';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      announcement: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      announcement: 'Here\'s a new announcement!'
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Announcer text={this.state.announcement} />
+        <div className="App-header">
+          <h2>Notification Example</h2>
+          <button type="button" onClick={this.handleClick}>Trigger new announcement</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('container'))
+
+if (module.hot) { module.hot.accept() }
